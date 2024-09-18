a7a3e6f1071f6dd0397ed1af80f38b7c65715ba8
SYED JALEES AKBAR SHAH <161203405+syedjalees@users.noreply.github.com>
2024-09-18 15:06:31 +0500

    Add files via upload

---
+++ Milestone-4/script.ts
@@ -0,0 +1,123 @@
+
+// alert("loading")
+function addNewWEField() {
+    //----------------------------------------------- console.log("Adding new field in work experience");
+  
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter Here");
+  
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+  
+    weOb!.insertBefore(newNode, weAddButtonOb);
+  }
+  
+  function addNewAQField() {
+    // ---------------------------------------console.log("Adding new field in academic Qualification");
+  
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("eqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter Here");
+  
+    let aqOb = document.getElementById("aq");
+    let aqAddButtonOb = document.getElementById("aqAddButton");
+  
+    aqOb!.insertBefore(newNode, aqAddButtonOb);
+  }
+  
+  //-------------------------------------------------------------generate resume
+  
+  function generateResume() {
+    let nameField = document.getElementById("nameField")!.value;
+  
+    let nameT1 = document.getElementById("nameT1");
+  
+    nameT1!.innerHTML = nameField;
+    //--------------------------------------------------------------------direct
+    //--------------------------------------------------------------------name
+    document.getElementById("nameT2")!.innerHTML = nameField;
+  
+    //-------------------------------------------------------------------contact
+    document.getElementById("contactT")!.innerHTML =
+      document.getElementById("contactField")!.value;
+  
+    //--------------------------------------------------------------address
+    document.getElementById("addressT")!.innerHTML =
+      document.getElementById("addressField")!.value;
+  
+    //------------------------------------------------------------- linkedIn-
+    document.getElementById("linkT")!.innerHTML =
+      document.getElementById("linkedinField")!.value;
+  
+    //----------------------------------------------------------------facebook
+    document.getElementById("fbT")!.innerHTML =
+      document.getElementById("fbField")!.value;
+  
+    //---------------------------------------------------------------objective
+    document.getElementById("objectiveT")!.innerHTML =
+      document.getElementById("objectiveField")!.value;
+  
+    //-----------------------------------------------------------work experience
+    let we = document.getElementsByClassName("weField");
+  
+    let str = "";
+  
+    for (let e of we) {
+      str = str + `<li> ${e.value} </li>`;
+    }
+    document.getElementById("weT")!.innerHTML = str;
+  
+    // ----------------------------------------------------academic Qualification
+    let aqs = document.getElementsByClassName("eqField");
+  
+    let str1 = "";
+  
+    for (let e of aqs) {
+      str1 = str1 + `<li> ${e.value} </li>`;
+    }
+    document.getElementById("aqT")!.innerHTML = str1;
+  
+    //----------------------------------------------------code for setting image
+  
+    let file = document.getElementById("imgField")!.files[0];
+  
+    console.log(file);
+  
+    let reader = new FileReader();
+  
+    reader.readAsDataURL(file);
+  
+    console.log(reader.result);
+  
+    //----------------------------------------------------set the image to template
+  
+    reader.onloadend = function () {
+      document.getElementById("imgTemplate")!.src = reader.result;
+    };
+  
+    document.getElementById("cv-form")!.style.display = "none";
+    document.getElementById("cv-template")!.style.display = "block";
+    
+  }
+  
+  //---------------------------------------------------------------- print resume
+  function printResume() {
+    window.print();
+  }
+  
+  // ----------------------------------------------------------------resume editing
+  function editResume() {
+    let editBtn = document.getElementById("editBtn");
+    editBtn!.style.display = editBtn!.style.display === "none" ? "block" : "none";
+  
+    document.getElementById("cv-form")!.style.display = "block";
+    document.getElementById("cv-template")!.style.display = "none";
+    document.getElementById("editBtn")!.style.display = "none";
+  }
---
+++ Milestone-5/script.js
@@ -0,0 +1,102 @@
+"use strict";
+// alert("loading")
+Object.defineProperty(exports, "__esModule", { value: true });
+//----------------------------------------------- Adding new field in work experience
+function addNewWEField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter Here");
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+    weOb.insertBefore(newNode, weAddButtonOb);
+}
+// ---------------------------------------------Adding new field in academic Qualification
+function addNewAQField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("eqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter Here");
+    let aqOb = document.getElementById("aq");
+    let aqAddButtonOb = document.getElementById("aqAddButton");
+    aqOb.insertBefore(newNode, aqAddButtonOb);
+}
+//-------------------------------------------------------------generate resume
+function generateResume() {
+    let name = document.getElementById("nameField").value;
+    let nameT1 = document.getElementById("nameT1");
+    nameT1.innerHTML = name;
+    //--------------------------------------------------------------------------direct
+    //---------------------------------------------------------------------------name
+    document.getElementById("nameT2").innerHTML = name;
+    //-------------------------------------------------------------------------contact
+    document.getElementById("contactT").innerHTML =
+        document.getElementById("contactField").value;
+    //------------------------------------------------------------------------address
+    document.getElementById("addressT").innerHTML =
+        document.getElementById("addressField").value;
+    //----------------------------------------------------------------------- linkedIn-
+    document.getElementById("linkT").innerHTML =
+        document.getElementById("linkedinField").value;
+    //-----------------------------------------------------------------------facebook
+    document.getElementById("fbT").innerHTML =
+        document.getElementById("fbField").value;
+    //------------------------------------------------------------------------objective
+    document.getElementById("objectiveT").innerHTML =
+        document.getElementById("objectiveField").value;
+    //-------------------------------------------------------------------work experience
+    let we = document.getElementsByClassName("weField");
+    let str = "";
+    for (let e of we) {
+        str = str + `<li>${e.value}</li>`;
+    }
+    document.getElementById("weT").innerHTML = str;
+    //-------------------------------------------------------------academic Qualification
+    let aqs = document.getElementsByClassName("eqField");
+    let str1 = "";
+    for (let e of aqs) {
+        str1 = str1 + `<li>${e.value}</li>`;
+    }
+    document.getElementById("aqT").innerHTML = str1;
+    //-----------------------------------------------------------------code for setting image
+    let file = document.getElementById("imgField").files[0];
+    // console.log(file);
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    // console.log(reader.result);
+    //----------------------------------------------------------------set the image to template
+    reader.onloadend = function () {
+        document.getElementById("imgTemplate").src = reader.result;
+    };
+    document.getElementById("cv-form").style.display = "none";
+    document.getElementById("cv-template").style.display = "block";
+    const username = name ? name.toLowerCase().replace(/\s+/g, '-') : 'user'; //Generate username from the name
+    const baseUrl = 'http://127.0.0.1:5500/index.html#1/'; // Your Vercel deployment URL
+    const uniqueResumeUrl = `${baseUrl}?/${username}`; // Create unique URL      
+    //-----------------------------------------------------------Set the resume link in the DOM
+    const resumeLink = document.getElementById('resumeLink');
+    resumeLink.setAttribute('href', uniqueResumeUrl);
+    resumeLink.textContent = uniqueResumeUrl;
+    //------------------------------------------------------Copy link to clipboard functionality
+    document.getElementById('copyLinkBtn').addEventListener('click', () => {
+        navigator.clipboard.writeText(uniqueResumeUrl).then(() => {
+            alert('Resume link copied to clipboard!');
+        });
+    });
+}
+//---------------------------------------------------------------- print resume
+function printResume() {
+    window.print();
+}
+// ----------------------------------------------------------------resume editing
+function editResume() {
+    let editBtn = document.getElementById("editBtn");
+    editBtn.style.display = editBtn.style.display === "none" ? "block" : "none";
+    document.getElementById("cv-form").style.display = "block";
+    document.getElementById("cv-template").style.display = "none";
+    document.getElementById("editBtn").style.display = "none";
+}
